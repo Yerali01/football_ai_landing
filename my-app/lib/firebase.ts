15f744b05b7fda93934ba33ab4e20a912c7eca20
eraliflash <eraliflash@gmail.com>
2026-01-11 14:54:27 +0500

    moved everything into .env file

--- my-app/lib/firebase.ts
+++ my-app/lib/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlbAL7M7-S0XgGo5LR6dqUkoXG1nbhRw0",
-  authDomain: "footballai-2bda2.firebaseapp.com",
-  databaseURL: "https://footballai-2bda2-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "footballai-2bda2",
-  storageBucket: "footballai-2bda2.firebasestorage.app",
-  messagingSenderId: "994616569056",
-  appId: "1:994616569056:web:b14363b599fd765e60f485",
-  measurementId: "G-JT4XBLKMNH"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
